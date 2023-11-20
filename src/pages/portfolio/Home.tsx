31ce3207a8409f796ee948bdf539e82d94076300
awanali <aliim.wanali@gmail.com>
2023-11-20 12:34:54 +0100

    Add decoration rectangle to mobile version

--- src/pages/portfolio/Home.tsx
+++ src/pages/portfolio/Home.tsx
@@ -26,7 +26,10 @@ export default function Home() {
 
 
             <section style={{ ...styles.about, ...(isMobile && { backgroundColor: '#161F22', padding: 24 }) }}>
-                <h1 style={{ fontSize: isMobile ? '2.145922746781116vh' : '1.224489795918367vw', fontWeight: 500, color: 'white', margin: 0 }}>/awanali</h1>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <h1 style={{ fontSize: isMobile ? '2.145922746781116vh' : '1.224489795918367vw', fontWeight: 500, color: 'white', margin: 0 }}>/awanali</h1>
+                    {isMobile && <img style={{ ...styles.sidebarImage, width: '2.145922746781116vh' }} src={targetSVG} />}
+                </div>
                 <h2 style={{ fontSize: isMobile ? '4.506437768240343vh' : '3.33333vw', fontWeight: 500, color: '#BDC1C2' }}>I <span style={{ color: '#1CB0F7' }}>code</span> games, {!isMobile && <br />} <span style={{ color: '#EB5A53' }}>design</span> 2D/3D assets {!isMobile && <br />} and produce <span style={{ color: '#FF9600' }}>music</span>.</h2>
                 <h3 style={{ fontSize: isMobile ? '2.575107296137339vh' : '1.66666vw', fontWeight: 'bold', color: 'white' }}>Hey im Wan Abdul Aliim Wanali.</h3>
                 <p style={{ fontSize: isMobile ? '2.145922746781116vh' : '1.224489795918367vw', fontWeight: 'normal', color: 'white' }}>I'm a software engineer looking for opportunities to gain experience in the industry. My interests ranges from building and designing human centered applications for web, desktop and mobile devices. Currently im working for the Lessing-Sts School in Hamburg, developing a gamification app for school kids.</p>
@@ -43,8 +46,8 @@ export default function Home() {
 
             {isMobile &&
                 <nav style={{ ...styles.navContainer, flexDirection: 'row', justifyContent: 'center', margin: 32 }}>
-                    <Link to={'/imprint'}  style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Imprint</Link>
-                    <Link to={'/privacy-policy'}  style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Privacy Policy</Link>
+                    <Link to={'/imprint'} style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Imprint</Link>
+                    <Link to={'/privacy-policy'} style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Privacy Policy</Link>
                 </nav>
             }
         </main>
