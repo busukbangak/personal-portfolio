7df8228f7cad1fb4fb89570763d2e55b3d674d52
awanali <aliim.wanali@gmail.com>
2023-11-23 10:59:42 +0100

    Add privacy policy and imprint to portfolio

--- src/pages/portfolio/PrivacyPolicy.tsx
+++ src/pages/portfolio/PrivacyPolicy.tsx
@@ -1,13 +1,16 @@
+import useWindowDimensions from '@/shared/hooks/useWindowDimensions';
 import StyleSheet from '@/shared/types/stylesheet';
-import { Link } from 'react-router-dom';
-
 
 import targetSVG from '@/assets/portfolio/target.svg'
-export default function Imprint() {
+import { Link } from 'react-router-dom';
+
+export default function PrivacyPolicy() {
+    const { width } = useWindowDimensions();
+    const isMobile = width < 1024;
 
     return (
-        <div className='container' style={styles.container}>
-            <main className='container' style={styles.container}>
+        <main className='container' style={{ ...styles.container, ...(isMobile && { flexDirection: 'column' }) }}>
+            {!isMobile &&
                 <aside style={styles.sidebar}>
                     <img style={styles.sidebarImage} src={targetSVG} />
                     <nav style={styles.navContainer}>
@@ -16,14 +19,25 @@ export default function Imprint() {
                         <Link to={'/privacy-policy'} style={styles.navText}>Privacy Policy</Link>
                     </nav>
                 </aside>
-                <section style={styles.content}>
-                    <h1 style={{}}>Privacy Policy</h1>
-                    <p style={{}}>
-                        text
-                    </p>
-                </section>
-            </main>
-        </div>
+            }
+
+
+            <section style={{ ...styles.content, ...(isMobile && { backgroundColor: '#161F22', padding: 24 }) }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <h1 style={{ fontSize: isMobile ? '2.145922746781116vh' : '1.224489795918367vw', fontWeight: 500, color: 'white', margin: 0 }}>/awanali</h1>
+                    {isMobile && <img style={{ ...styles.sidebarImage, width: '2.145922746781116vh' }} src={targetSVG} />}
+                </div>
+                <h2 style={{ fontSize: isMobile ? '4.506437768240343vh' : '3.33333vw', fontWeight: 500, color: '#BDC1C2' }}>Privacy Policy</h2>
+                <p style={{ fontSize: isMobile ? '2.145922746781116vh' : '1.224489795918367vw', fontWeight: 'normal', color: 'white' }}>We dont collect any data on this website.</p>
+            </section>
+
+            {isMobile &&
+                <nav style={{ ...styles.navContainer, flexDirection: 'row', justifyContent: 'center', margin: 32 }}>
+                    <Link to={'/imprint'} style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Imprint</Link>
+                    <Link to={'/privacy-policy'} style={{ ...styles.navText, writingMode: 'unset', fontSize: '1.716738197424893vh' }}>Privacy Policy</Link>
+                </nav>
+            }
+        </main>
     )
 }
 
@@ -31,20 +45,21 @@ const styles = StyleSheet.create({
     container: {
         display: 'flex',
         flex: 1,
-        backgroundColor: 'gainsboro',
+        backgroundColor: '#161F22',
         flexDirection: 'row',
         overflow: 'auto',
+        fontFamily: 'Montserrat'
     },
     sidebar: {
         display: 'flex',
         flexDirection: 'column',
-        width: 75,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        width: '3.90625vw'
     },
     sidebarImage: {
-        width: '45%',
+        width: '35%',
         alignSelf: 'center',
-        margin: 32
+        margin: '1.66666vw'
     },
     navContainer: {
         display: 'flex',
@@ -55,14 +70,16 @@ const styles = StyleSheet.create({
         writingMode: 'vertical-lr',
         textOrientation: 'sideways',
         textDecoration: 'none',
-        margin: 32,
-        cursor: 'pointer'
+        color: '#fff',
+        cursor: 'pointer',
+        margin: '1.66666vw',
+        fontSize: '0.8163265306122449vw'
     },
     content: {
-        flex: 1,
-        backgroundColor: 'gray',
-        width: 750,
-        padding: 32
-    }
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#232F35',
+        flex: 3,
+        padding: '1.66666vw'
+    },
 });
-
