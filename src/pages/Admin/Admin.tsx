33cb5a606c52eec01872c8e566d51483262623ac
awanali <aliim.wanali@gmail.com>
2025-12-12 16:39:49 +0100

    Remove Admin component and redirect logic from Admin.tsx

--- src/pages/Admin/Admin.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function Admin() {
-    useEffect(() => {
-        window.location.href = '/admin/index.html';
-    }, []);
-
-    return <div>Redirecting to admin panel...</div>;
-}
