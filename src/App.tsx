3808427bdcaf7405149fd1f4ea730a021aa5e6aa
awanali <aliim.wanali@gmail.com>
2025-12-12 16:35:54 +0100

    Remove admin route and associated script from index.html

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
 import Imprint from '@/pages/Imprint/Imprint';
 import PrivacyPolicy from '@/pages/PrivacyPolicy/PrivacyPolicy';
-import Admin from '@/pages/Admin/Admin';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
@@ -21,9 +20,6 @@ export default function App() {
             <Route path='/tags/:mode/:tags' Component={Home} />
             <Route path='/imprint' Component={Imprint} />
             <Route path='/privacy-policy' Component={PrivacyPolicy} />
-
-            {/* Admin - Redirect to static admin page */}
-            <Route path='/admin' Component={Admin} />
 
             {/* Misc */}
             <Route path='/*' element={<Navigate to='/' />} />
